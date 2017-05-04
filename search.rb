ae8fe3da50173050073246220f4c02e75e826840
Siddharth Sharma <svs@svs.io>
2017-05-04 09:49:10 +0530

    adds must_not clause

--- search.rb
+++ search.rb
@@ -7,7 +7,17 @@ client = Elasticsearch::Client.new
 r = rand(1000)
 ap "searching for: #{r}"
 5.times do
-  s = client.search index: 'test', type: 'profile', body: {query: {constant_score: {filter: { terms: { seen: [r]}}}}}
+  s = client.search index: 'test', type: 'profile', body: {
+                      query: {
+                        bool: {
+                          must_not: {
+                            term: {
+                              seen: r
+                            }
+                          }
+                        }
+                      }
+                    }
   s["hits"] = s["hits"]["total"]
   ap "took #{s["took"]} ms for #{s["hits"]} hits"
 end
