6e29bf4cab37b731ea3626725a8d85581c3548b1
Siddharth Sharma <svs@svs.io>
2017-05-05 12:44:25 +0530

    adds scoring and more conditions

--- create_data.rb
+++ create_data.rb
@@ -3,6 +3,12 @@ Bundler.setup
 require 'elasticsearch'
 require 'awesome_print'
 
+class Range
+  def sample
+    self.to_a.sample
+  end
+end
+
 client = Elasticsearch::Client.new
 ap client.cluster.health
 
@@ -11,16 +17,58 @@ jobs = JSON.load(File.open("./occupations.json"))["occupations"]
 names = JSON.load(File.open("./names.json"))["names"]
 
 
-client.indices.delete index: 'test'
+client.indices.delete index: 'test' rescue nil
 
 t = Time.new
-1_00_000.times do |i|
-  b = {name: names.sample, caste: castes.sample, job: jobs.sample}
+n = 1_00_000
+@q = Queue.new
+n.times do |i|
+  b = {
+    religion: ['Yogi','Bhogi','Rogi','Jedi','Pastafarian'].sample,
+    name: names.sample,
+    caste: castes.sample,
+    caste_number: rand(60000),
+    date_of_birth: Date.today - rand(3650) - (18*365),
+    height: 55 + rand(15),
+    weight: 45 + rand(55),
+    income: [0,1,3,6,10,20,40,60,100,1000].sample,
+    marital_status: (0..3).sample,
+    manglik: rand(1),
+    mothertongue: rand(50),
+    children: [true,false].sample,
+    drink: (0..2).sample,
+    smoke: (0..2).sample,
+    job: jobs.sample,
+    job_number: rand(111),
+    diet: (0..2).sample,
+    special_cases: (0..2).sample,
+    screened: [true, false].sample,
+    hidden: [true, false].sample,
+    last_login_date: Date.today - rand(90),
+  }
   b[:seen] = (Array.new(rand(10000)) { rand(100000) }).uniq.compact
-  x = client.index index: 'test', type: 'profile', body: b
-  if i%100 == 0
-    ap i
+  @q.push(b)
+  if @q.length%1000 == 0
+    p @q.length
   end
 end
+p "Done pushing #{n} in #{ap Time.now - t}"
+@threads = Array.new(8) do
+  Thread.new do
+    until @q.empty? do
+      l = @q.length
+      o = @q.shift
+      x = client.index index: 'test', type: 'profile', body: o
+      if l%100 == 0
+        ap "#{l} #{Time.now - t}"
+      end
+    end
+  end
+end
+
+@threads.each(&:join)
 ap Time.now - t
+
+
+
 #ap client.search index: 'test'
--- search.rb
+++ search.rb
@@ -3,21 +3,110 @@ Bundler.setup
 require 'pry'
 require 'elasticsearch'
 require 'awesome_print'
+
+class Range
+  def sample
+    self.to_a.sample
+  end
+end
+
 client = Elasticsearch::Client.new
-r = rand(1000)
+r = rand(10000)
 ap "searching for: #{r}"
-5.times do
-  s = client.search index: 'test', type: 'profile', body: {
-                      query: {
-                        bool: {
-                          must_not: {
-                            term: {
-                              seen: r
-                            }
-                          }
-                        }
-                      }
-                    }
+stats = []
+t = rand(125) + 10
+caste_range = (1 + rand(5)) * 10000
+job_range = rand(11) * 10
+
+must = [
+  {
+    term: {
+      manglik: 0
+    }
+  },
+  {
+    range: {
+      caste_number: {
+        gte: caste_range,
+        lte: caste_range + 9999,
+        boost: 2
+      }
+    }
+  },
+  {
+    range: {
+      date_of_birth: {
+        gte: "now-27y",
+        lte: "now-18y",
+        boost: 2
+      }
+    }
+  },
+  {
+    term: {
+      income: [0,1,3,6,10,20,40,60,100,1000].sample
+    }
+  },
+  {
+    term: {
+      marital_status: (0..3).sample,
+    }
+  },
+  {
+    range: {
+      job_number: {
+        gte: job_range,
+        lte: job_range + 9,
+        boost: 5
+      }
+    }
+  },
+  {
+    range: {
+      height: {
+        gte: 52,
+        lte: 66
+      }
+    }
+  }
+]
+
+
+
+body = {
+  query: {
+    function_score: {
+      query: {
+        bool: {
+          must_not: [
+            term: {
+              seen: r
+            }
+          ],
+          must: []
+
+        }
+      },
+      functions: [
+        {
+          gauss: {
+            caste_number: {
+              origin: caste_range + rand(500) + 250,
+              scale: caste_range - 1,
+              decay: 0.001
+            }
+          }
+        }
+      ]
+    }
+  }
+}
+
+ap body
+t.times do
+  s = client.search index: 'test', type: 'profile', body: body
   s["hits"] = s["hits"]["total"]
+  stats << s["took"]
   ap "took #{s["took"]} ms for #{s["hits"]} hits"
 end
+ap stats.reduce(:+)/(t * 1.0)
